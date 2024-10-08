107414483bbed889941c6bfb668ecd6b3607c965
schiavoanto <scalarino04@gmail.com>
2024-10-08 02:19:04 +0200

    Layout changes
    - Hover effects on projects images and social link icons
    - Changed layout to be better on various screen sizes (and mobile too)

--- index.js
+++ index.js
@@ -7,13 +7,18 @@ let zoomDialogImage;
 function zoomImage(imageSource) {
     zoomDialog.showModal();
     zoomDialogImage.setAttribute("src", imageSource);
+    zoomDialogImage.setAttribute("open", "true");
 }
 
 function closeDialog() {
     zoomDialog.close();
+    zoomDialogImage.setAttribute("open", "false");
 }
 
 window.onload = () => {
     zoomDialog = document.getElementById("zoom-dialog");
     zoomDialogImage = document.getElementById("zoom-dialog-image");
+
+    let ageSpan = document.getElementById("age-span");
+    ageSpan.innerText = (new Date(Date.now() - new Date(2004, 9, 7, 0, 0, 0, 0)).getUTCFullYear() - 1970);
 };
