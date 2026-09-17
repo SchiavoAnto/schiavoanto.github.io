99e00a6760f0bbc8882af5c3341c21add626e6c7
schiavoanto <scalarino04@gmail.com>
2026-09-17 02:34:21 +0200

    Remove age info tooltip

--- index.js
+++ index.js
@@ -30,25 +30,6 @@ function dialogClick(event) {
     }
 }
 
-function ageSpanClick() {
-    const dst = ageSpan.getAttribute("data-show-tooltip");
-    if (dst === null) return;
-    ageSpan.setAttribute("data-show-tooltip", dst === "false" ? "true" : "false");
-    repositionAgeSpanTooltip();
-}
-
-function repositionAgeSpanTooltip() {
-    const ageSpanAfter = getComputedStyle(ageSpan, ":after");
-    const rect = ageSpan.getBoundingClientRect();
-
-    let side = 1;
-    if (rect.x + parseFloat(ageSpanAfter.width) > document.documentElement.clientWidth) {
-        side = -1;
-    }
-
-    ageSpan.setAttribute("data-side", side);
-}
-
 window.onload = () => {
     zoomDialog = document.getElementById("zoom-dialog");
     zoomDialogImage = document.getElementById("zoom-dialog-image");
@@ -57,9 +38,4 @@ window.onload = () => {
 
     ageSpan = document.getElementById("age-span");
     ageSpan.innerText = (new Date(Date.now() - new Date(2004, 9, 7, 0, 0, 0, 0)).getUTCFullYear() - 1970);
-    ageSpan.addEventListener("click", ageSpanClick);
-
-    window.addEventListener("resize", () => {
-        repositionAgeSpanTooltip();
-    });
 };
